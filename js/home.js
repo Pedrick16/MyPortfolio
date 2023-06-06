83c7ed57f80a7353866cb55b1632a6fd84d29940
Pedrick16 <pedrickantazo@gmail.com>
2023-06-06 17:27:47 +0800

    add some codes

--- js/home.js
+++ js/home.js
@@ -1,5 +1,6 @@
 const icon = document.querySelector('#icon');
 const menuContainer = document.querySelector('.menu-container');
+const clickMenu = document.querySelectorAll('#click-menu')
 
 icon.addEventListener('click',()=>{
     if (icon.classList.contains('fa-bars')) {
@@ -7,13 +8,38 @@ icon.addEventListener('click',()=>{
         icon.classList.add('fa-close')
 
         menuContainer.classList.remove('hide')
-        menuContainer.classList.add('whow')
+        menuContainer.classList.add('show')
     } else {
         icon.classList.add('fa-bars')
         icon.classList.remove('fa-close')
 
         menuContainer.classList.add('hide')
-        menuContainer.classList.remove('whow')
+        menuContainer.classList.remove('show')
         
     }
 })
+
+
+clickMenu.forEach(function(element) {
+    element.addEventListener('click', function() {
+      if(icon.classList.contains('fa-close')) {
+        icon.classList.remove('fa-close');
+        icon.classList.add('fa-bars');
+  
+        menuContainer.classList.add('hide');
+        menuContainer.classList.remove('show');
+      } else {
+        icon.classList.add('fa-bars');
+        icon.classList.remove('fa-close');
+  
+        menuContainer.classList.add('show');
+        menuContainer.classList.remove('hide');
+      }
+    });
+  });
+
+
+
+
+
+
